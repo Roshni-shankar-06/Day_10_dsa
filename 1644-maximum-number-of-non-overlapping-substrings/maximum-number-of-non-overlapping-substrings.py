56249ccc1383181972b653612b57d396dfd0839c
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-18 12:17:18 +0530

    Remove initial implementation of maxNumOfSubstrings

--- 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.py
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.py
@@ -1,6 +1,1 @@
-class Solution:
-    def maxNumOfSubstrings(self, s: str) -> list[str]:
-        # Step 1: Record the first and last occurrence of each character
-        first = {c: i for i, c in enumerate(s)}
-        last = {c: i for i, c in enumerate(s)}
-     
+
