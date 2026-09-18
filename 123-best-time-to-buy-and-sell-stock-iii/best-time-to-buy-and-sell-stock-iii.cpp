0e8a9b97531b4cf1f18ca35f70043966dd4dc535
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-18 12:19:25 +0530

    Refactor maxProfit by removing first transaction variables
    Removed variables for the first transaction to simplify the logic.

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,10 +3,6 @@ class Solution {
   int maxProfit(vector<int>& prices) {
     int sellTwo = 0;
     int holdTwo = INT_MIN;
-    int sellOne = 0;
-    int holdOne = INT_MIN;
-
-    for (const int price : prices) {
-     
+   
    
    
