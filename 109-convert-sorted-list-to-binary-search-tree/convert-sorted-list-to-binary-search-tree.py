0b14ba62c54854cb8c6e61f7345f4232a4d04e85
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-18 12:30:45 +0530

    Remove linked list to array conversion in sortedListToBST
    Removed the linked list conversion logic from the sortedListToBST method.

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
@@ -2,11 +2,6 @@ class Solution:
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
         # Step 1: Convert the linked list into an array
         values = []
-        while head:
-            values.append(head.val)
-            head = head.next
-            
-        # Step 2: Recursively build the BST from the array
-      
+       
        
          
