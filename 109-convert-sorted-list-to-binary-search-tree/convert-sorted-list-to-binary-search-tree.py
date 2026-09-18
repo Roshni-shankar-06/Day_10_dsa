37dc070678cb811a18e44688a32da5803447311b
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-18 12:30:56 +0530

    Remove initial method implementation
    Removed initial implementation of sortedListToBST method.

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
@@ -1,7 +1,3 @@
-class Solution:
-    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        # Step 1: Convert the linked list into an array
-        values = []
-       
+
        
          
