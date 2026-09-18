14c1ff4f193abadcda14deb5a1c32afeb81f3222
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-18 12:28:26 +0530

    Remove dfs function from pathSum method
    Removed the depth-first search (dfs) function from pathSum method.

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -2,11 +2,5 @@ class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         result = []
         
-        def dfs(node, current_sum, path):
-            if not node:
-                return
-            
-            # Include the current node in the path
-            path.append(node.val)
-            
+       
            
